137a92cf265dc7d3e3a4e9547771335cc01c7d9a
gyarmatiadam <gyari12@gmail.com>
2024-03-29 11:58:18 +0100

    Add Address entity and liquibase config

---
+++ ponte-exercise-backend/src/main/java/com/gyarmati/ponteexercisebackend/domain/Address.java
@@ -0,0 +1,27 @@
+package com.gyarmati.ponteexercisebackend.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    private String city;
+
+    private String street;
+
+    @Column(name = "house_number")
+    private String houseNumber;
+}
---
+++ ponte-exercise-backend/src/main/resources/db/changelog/scripts/create_address_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE address (
+                         id SERIAL PRIMARY KEY,
+                         zip_code VARCHAR(255),
+                         city VARCHAR(255),
+                         street VARCHAR(255),
+                         house_number VARCHAR(255)
+)
