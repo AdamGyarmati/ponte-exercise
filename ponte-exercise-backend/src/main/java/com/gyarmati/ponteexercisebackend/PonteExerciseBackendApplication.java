4a4cc7a140516289df0d414cff375d539a0711c8
gyarmatiadam <gyari12@gmail.com>
2024-03-29 10:48:53 +0100

    Add existing project files to Git

---
+++ ponte-exercise-backend/src/main/java/com/gyarmati/ponteexercisebackend/PonteExerciseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gyarmati.ponteexercisebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonteExerciseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PonteExerciseBackendApplication.class, args);
+    }
+
+}
---
+++ ponte-exercise-backend/src/test/java/com/gyarmati/ponteexercisebackend/PonteExerciseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyarmati.ponteexercisebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonteExerciseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
