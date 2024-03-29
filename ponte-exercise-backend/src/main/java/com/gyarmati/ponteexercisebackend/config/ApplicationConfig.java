6d12479e093a6e27d15be4bce2e7adf7b3115ba6
gyarmatiadam <gyari12@gmail.com>
2024-03-29 14:18:49 +0100

    Config basic security

---
+++ ponte-exercise-backend/src/main/java/com/gyarmati/ponteexercisebackend/config/ApplicationConfig.java
@@ -0,0 +1,31 @@
+package com.gyarmati.ponteexercisebackend.config;
+
+import com.gyarmati.ponteexercisebackend.service.AppUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final AppUserService appUserService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(appUserService);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
